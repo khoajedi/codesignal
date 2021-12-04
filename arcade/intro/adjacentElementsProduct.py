0248ad9cfe8c6ee1cdb04c64af440c94cadd93c3
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-04 11:04:20 +0700

    Update adjacentElementsProduct python3

--- arcade/intro/adjacentElementsProduct.py
+++ arcade/intro/adjacentElementsProduct.py
@@ -5,3 +5,6 @@ def solution(inputArray):
         if prod > max:
             max = prod
     return max
+
+def solutionV2(inputArray):
+    return max(a * b for a, b in zip(inputArray[:-1], inputArray[1:]))
