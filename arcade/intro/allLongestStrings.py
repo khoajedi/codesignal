4acf3e32474193dc176ed189cf0fa94551785a8b
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-05 22:16:30 +0700

    Update allLongestStrings python3

--- arcade/intro/allLongestStrings.py
+++ arcade/intro/allLongestStrings.py
@@ -1,3 +1,7 @@
 def solution(inputArray):
     m = max(len(x) for x in inputArray)
     return list(x for x in inputArray if len(x) == m)
+
+def solutionV2(inputArray):
+    m = max(len(x) for x in inputArray)
+    return [x for x in inputArray if len(x) == m]
