ae8914fb686cb71f0ee734e4501f0f6257085fdd
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-19 09:09:24 +0700

    Update differentSquares python3

--- arcade/intro/differentSquares.py
+++ arcade/intro/differentSquares.py
@@ -9,3 +9,12 @@ def solution(m):
             a.append(s)
             
     return len(set(a))
+
+def solutionV2(m):
+    s = set()
+    
+    for i in range(len(m) - 1):
+        for j in range(len(m[0]) - 1):
+            s.add((m[i][j], m[i][j+1], m[i+1][j], m[i+1][j+1]))
+            
+    return len(s)
