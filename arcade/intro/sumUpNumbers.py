e942731320cf266ce5503b2014f9e04771e1d078
Khoa D. Vo <khoa.jedi@gmail.com>
2021-12-19 09:01:34 +0700

    Update sumUpNumbers python3

--- arcade/intro/sumUpNumbers.py
+++ arcade/intro/sumUpNumbers.py
@@ -1,3 +1,7 @@
 def solution(inputString):
-    a = re.findall("(\d*)", inputString)
+    a = re.findall("\d+", inputString)
+    return sum(int(e) for e in a if e.isdigit())
+
+def solutionV2(inputString):
+    a = re.findall("\d+", inputString)
     return sum(int(e) for e in a if e.isdigit())
